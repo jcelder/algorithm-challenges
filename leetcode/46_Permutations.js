2da4528f45f6fbcf30c254df45ea11e9312c05d1
Joshua Elder <jcelder88@gmail.com>
2018-12-20 15:26:21 -0800

    Return all possible permutations

--- leetcode/46_Permutations.js
+++ leetcode/46_Permutations.js
@@ -17,3 +17,21 @@
 // O - Array of permutations (Arrays)
 // C - None
 // E - Empty input
+
+var permute = function(nums) {
+  let permutations = [];
+  
+  const recurse = (permutationSoFar, remainingNums) => {
+      if (remainingNums.length === 0) {
+          permutations.push(permutationSoFar);
+      } else {
+          for (let i = 0; i < remainingNums.length; i += 1) {
+
+              recurse(permutationSoFar.concat(remainingNums[i]), remainingNums.slice(0, i).concat(remainingNums.slice(i + 1)));
+          }
+      }
+  }
+  recurse([], nums);
+  
+  return permutations;
+};
