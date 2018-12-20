942065c423c95955fe26c743883a40bf1c3b292d
Joshua Elder <jcelder88@gmail.com>
2018-12-20 15:25:55 -0800

    Initial commit

---
+++ leetcode/46_Permutations.js
@@ -0,0 +1,19 @@
+// Given a collection of distinct integers, return all possible permutations.
+
+// Example:
+
+// Input: [1,2,3]
+// Output:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+// I - Array of integers
+// O - Array of permutations (Arrays)
+// C - None
+// E - Empty input
